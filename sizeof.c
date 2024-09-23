d9891d636df2c81c6bd7881be2ec7960d766e46f
vboxuser <vboxuser@vbox.syspr.virtualbox.org>
2024-09-23 14:49:33 +0200

    print sizeof of datatypes

---
+++ sizeof.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("sizeof(char) = %lu\n", sizeof(char));
+    printf("sizeof(short) = %lu\n", sizeof(short));
+    printf("sizeof(int) = %lu\n", sizeof(int));
+    printf("sizeof(unsigned int) = %lu\n", sizeof(unsigned int));
+    printf("sizeof(long) = %lu\n", sizeof(long));
+    printf("sizeof(unsigned long) = %lu\n", sizeof(unsigned long));
+    printf("sizeof(long long) = %lu\n", sizeof(long long));
+    printf("sizeof(float) = %lu\n", sizeof(float));
+    printf("sizeof(double) = %lu\n", sizeof(double));
+    return 0;
+}
